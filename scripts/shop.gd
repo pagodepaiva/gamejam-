017bac4951c7224f4c1018b11743a1157e3aee54
Rhavy Frota <rhavy.souza1@hp.com>
2024-10-20 19:51:04 -0300

    Started menus

---
+++ scripts/shop.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var money : int = 0
+
+
+var fishs = [0,0,0,0,0,0]
+
+"""
+1 - nemo
+1 - fogheche
+1 - catfish
+1 - sea worm
+1 - unifish
+1 - spin fish
+
+"""
---
+++ scripts/start.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+
+func _on_q_button_pressed() -> void:
+	get_tree().quit()
